6e38fec42d87ec7893bf7996f45fa84fcc90deef
Vaayuf <vaayuf@gmail.com.com>
2021-01-05 05:47:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [city, setCity] = useState('null');
+  const [search, setSearch] = useState();
+
+  const searchValue = (event) => {
+    setSearch(event.target.value);
+  }
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=c3cdc9af5a63d41e4cbce38f9e43f06f`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      setCity(resJson.main);
+    };
+
+    fetchApi();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="box">
+        <div className="card">
+          <div className="inputData">
+            <input type="text" placeholder="Search" value={search} onChange={searchValue} />
+          </div>
+
+          {!city ?
+            (
+              <p>No Data found</p>
+            ) :
+            (
+              <div className="info">
+                <h1 className="location">
+                  <span class="material-icons">
+                    location_on
+                  </span>
+                   {search} 
+                  
+                </h1>
+
+                <h2 className="temp">
+                   {city.temp} 
+                  25.49
+                </h2>
+                <h3 className="extra-info">
+                  Min temp: {city.temp_min} | Max temp: {city.temp_max}
+                </h3>
+              </div>
+            )}
+        </div>
+      </div>
+    </>
   );
 }
 
